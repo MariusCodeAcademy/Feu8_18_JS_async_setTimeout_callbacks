fac0f4608e95edc882744a42d7624bf074c46557
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-20 09:35:36 +0300

    setTimeout

--- js/setTimeout.js
+++ js/setTimeout.js
@@ -1,2 +1,19 @@
 'use strict';
 console.log('setTimeout.js file was loaded');
+
+const btnEl = document.getElementById('btn');
+
+// setTimeout(ka paleisti, laikas milisek)
+// paleisti procesa po kazkiek laiko
+setTimeout(() => {
+  console.log('i am inside setTimeout');
+}, 3000);
+
+console.log('i am first');
+let count = 0;
+btnEl.addEventListener('click', () => {
+  console.log('paspaudei', count++);
+});
+
+// setInterval(ka paleisti, laikas milisek)
+// paleisti procesa kas kazkiek laiko
