0330dc21ff374ed75ba94737dfeb35047ee7156a
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-20 09:52:53 +0300

    setInterval + clock

--- js/setTimeout.js
+++ js/setTimeout.js
@@ -2,6 +2,7 @@
 console.log('setTimeout.js file was loaded');
 
 const btnEl = document.getElementById('btn');
+const timeEl = document.getElementById('time');
 
 // setTimeout(ka paleisti, laikas milisek)
 // paleisti procesa po kazkiek laiko
@@ -12,8 +13,32 @@ setTimeout(() => {
 console.log('i am first');
 let count = 0;
 btnEl.addEventListener('click', () => {
-  console.log('paspaudei', count++);
+  logKazka();
 });
 
 // setInterval(ka paleisti, laikas milisek)
 // paleisti procesa kas kazkiek laiko
+// const interval1 = setInterval(logKazka, 2000);
+
+function logKazka() {
+  console.log('paspaudei', ++count);
+  if (count >= 10) {
+    console.log('stop');
+    // sutabdyti intervala
+    clearInterval(interval1);
+  }
+}
+
+setInterval(tick, 1000);
+tick();
+
+function tick() {
+  const nowObj = new Date();
+  // console.log('nowObj ===', nowObj);
+  const formatedTime = nowObj.toLocaleString('lt', {
+    // dateStyle: 'full',
+    timeStyle: 'medium',
+  });
+  // console.log('formatedTime ===', formatedTime);
+  timeEl.textContent = formatedTime;
+}
