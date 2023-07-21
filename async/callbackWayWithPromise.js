1bc988a502018c12d423248ca7f4cc2ddc47398a
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-21 09:19:48 +0300

    Promisify fn

--- async/callbackWayWithPromise.js
+++ async/callbackWayWithPromise.js
@@ -5,32 +5,41 @@ console.log('callbackWay.js file was loaded');
 
 // sakysim kad mums reikia pagaminti objekta su username, town, age
 
-function getUsername(callBack) {
-  console.log('pries timeout');
-  setTimeout(() => {
-    console.log('1. vardas: James');
-    // kviesti miesta tik kai turim varda
-    callBack();
-  }, 500);
+function getUsername() {
+  return new Promise((resolve, reject) => {
+    console.log('pries timeout');
+    setTimeout(() => {
+      console.log('1. vardas: James');
+      // kviesti miesta tik kai turim varda
+      resolve();
+    }, 1500);
 
-  console.log('po timeout');
+    console.log('po timeout');
+  });
 }
-function getTown(callBack) {
-  setTimeout(() => {
-    console.log('2. miestas: London');
-    // kviesti getAge tik kai gavom miesta
-    callBack();
-  }, 2500);
+function getTown() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('2. miestas: London');
+      // kviesti getAge tik kai gavom miesta
+      resolve();
+    }, 2500);
+  });
 }
 function getAge() {
-  setTimeout(() => {
-    console.log('3. amzius: 47');
-  }, 300);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('3. amzius: 47');
+    }, 300);
+  });
 }
 
 // jei butu daugiau lygiu tai yra vadinama 'callback hell'
-getUsername(() => {
-  getTown(() => {
-    getAge();
-  });
-});
+// getUsername(() => {
+//   getTown(() => {
+//     getAge();
+//   });
+// });
+getUsername()
+  .then(() => getTown())
+  .then(() => getAge());
